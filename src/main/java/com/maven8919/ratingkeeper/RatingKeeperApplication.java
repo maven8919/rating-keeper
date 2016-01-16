0fd6494f8025f121eb7e254baf6ee06fd11f9780
maven8919 <maven8919@gmail.com>
2016-01-16 18:54:34 +0100

    Initial commit

---
+++ src/main/java/com/maven8919/ratingkeeper/RatingKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.maven8919.ratingkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingKeeperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maven8919/ratingkeeper/RatingKeeperApplicationTests.java
@@ -0,0 +1,18 @@
+package com.maven8919.ratingkeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RatingKeeperApplication.class)
+@WebAppConfiguration
+public class RatingKeeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
