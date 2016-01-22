bedf35fac04dea659a82781144141b56ea73189a
Levente Bakos <levente_bakos@epam.com>
2016-01-22 17:03:00 +0100

    Add social properties

--- src/main/java/com/maven8919/ratingkeeper/controller/IndexController.java
+++ src/main/java/com/maven8919/ratingkeeper/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.maven8919.ratingkeeper.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,8 +11,22 @@ public class IndexController {
 
 	private static final String REQUEST_MAPPING_FOR_INDEX = "/";
 	private static final String INDEX_LOGICAL_VIEW = "index";
+	
+	private Facebook facebook;
 
-	@RequestMapping(REQUEST_MAPPING_FOR_INDEX)
+	@Autowired
+	public IndexController(Facebook facebook) {
+        super();
+        this.facebook = facebook;
+    }
+	
+	@ModelAttribute(value="name")
+	public String facebookName() {
+	    return facebook.userOperations().getUserProfile().getFirstName();
+	}
+	
+
+    @RequestMapping(REQUEST_MAPPING_FOR_INDEX)
 	public String index() {
 		return INDEX_LOGICAL_VIEW;
 	}
