809a757401ef4f718c9e9c3bde9a6723bb7910b9
maven8919 <maven8919@gmail.com>
2016-02-03 22:38:02 +0100

    Finish registration

--- src/main/java/com/maven8919/ratingkeeper/user/controller/UserCreatePostController.java
+++ src/main/java/com/maven8919/ratingkeeper/user/controller/UserCreatePostController.java
@@ -5,6 +5,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,18 +15,26 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.maven8919.ratingkeeper.user.model.AddUserRequest;
 import com.maven8919.ratingkeeper.user.service.UserService;
 import com.maven8919.ratingkeeper.user.transformer.AddUserRequestToUserEntityTransformer;
+import com.maven8919.ratingkeeper.user.validation.AddUserRequestValidatior;
 
 @Controller
 public class UserCreatePostController {
     
     private UserService userService;
     private AddUserRequestToUserEntityTransformer addUserRequestToUserEntityTransformer;
+    private AddUserRequestValidatior addUserRequestValidator;
     
     @Autowired
-    public UserCreatePostController(UserService userService, AddUserRequestToUserEntityTransformer addUserRequestToUserEntityTransformer) {
+    public UserCreatePostController(UserService userService, AddUserRequestToUserEntityTransformer addUserRequestToUserEntityTransformer, AddUserRequestValidatior addUserRequestValidator) {
         super();
         this.userService = userService;
         this.addUserRequestToUserEntityTransformer = addUserRequestToUserEntityTransformer;
+        this.addUserRequestValidator = addUserRequestValidator;
+    }
+    
+    @InitBinder("addUserRequest")
+    public void initBinder(WebDataBinder binder) {
+        binder.addValidators(addUserRequestValidator);
     }
     
     @RequestMapping(value = "/user/create", method = RequestMethod.POST)
--- src/main/java/com/maven8919/ratingkeeper/user/validation/AddUserRequestValidatior.java
+++ src/main/java/com/maven8919/ratingkeeper/user/validation/AddUserRequestValidatior.java
@@ -1,14 +1,48 @@
 package com.maven8919.ratingkeeper.user.validation;
 
-import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
-import javax.validation.executable.ExecutableValidator;
-import javax.validation.metadata.BeanDescriptor;
+import com.maven8919.ratingkeeper.user.model.AddUserRequest;
+import com.maven8919.ratingkeeper.user.service.UserService;
 
-public class AddUserRequestValidatior {
+@Component
+public class AddUserRequestValidatior implements Validator {
+	
+	private UserService userService;
+	
+	@Autowired
+	public AddUserRequestValidatior(UserService userService) {
+		super();
+		this.userService = userService;
+	}
 
-    
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return clazz.equals(AddUserRequest.class);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		AddUserRequest addUserRequest = (AddUserRequest) target;
+		validatePasswords(addUserRequest, errors);
+		validateUsername(addUserRequest, errors);
+	}
+
+	private void validateUsername(AddUserRequest addUserRequest, Errors errors) {
+		String password = addUserRequest.getPassword();
+		String passwordConfirmation = addUserRequest.getPasswordConfirmation();
+		if (!password.equals(passwordConfirmation)) {
+			errors.reject("passwordsnotmatching", "The passwords are not matching, please review.");
+		}
+	}
+
+	private void validatePasswords(AddUserRequest addUserRequest, Errors errors) {
+		if (userService.getUserByUsername(addUserRequest.getUsername().trim()).isPresent()) {
+			errors.reject("usernametaken", "The " + addUserRequest.getUsername().trim() + " handle is taken, please choose another one.");
+		}
+	}
 
 }
